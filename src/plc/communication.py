8433944828e0ea4337182e31ef02cfa7276a5107
lg10migue <miguel_lopez@wiwynn.com>
2024-03-01 10:26:42 -0700

    Update the PLC communication file.
    Added multithreading support for sending signals to the PLC.

--- src/plc/communication.py
+++ src/plc/communication.py
@@ -1,6 +1,7 @@
 from pycomm3 import LogixDriver
 import time
 from config import PLC_3L3, PLC_3L6
+import threading
 
 
 def main():
@@ -8,15 +9,21 @@ def main():
 
 
 def send_signal(robot, line, signal=True):
-    plc_ip = PLC_3L3 if line == "3L3" else PLC_3L6
-    tag = ("OK_" if signal else "NG_") + str(robot)
-    try:
-        with LogixDriver(plc_ip) as plc:
-            plc.write(tag, True)
-            time.sleep(1)
-            plc.write(tag, False)
-    except Exception as e:
-        return
+    def task():
+        plc_ip = PLC_3L3 if line == "3L3" else PLC_3L6
+        tag = ("OK_" if signal else "NG_") + str(robot)
+        try:
+            with LogixDriver(plc_ip) as plc:
+                plc.write(tag, True)
+                time.sleep(1)
+                plc.write(tag, False)
+        except Exception as e:
+            return
+
+    # Start the task in a new thread.
+    thread = threading.Thread(target=task)
+    thread.daemon = True
+    thread.start()
 
 
 if __name__ == "__main__":
